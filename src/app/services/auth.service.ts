0970a5086d392a58d06b6193ff4ca0c41bf6635b
Oleksandr-Mykhailovskyi <Dadox56@gmail.com>
2025-10-10 20:16:02 +0200

    configuration

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable, tap } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://strapi-cloud-template-blog-ab0ea8f6c9.onrender.com';
+  private apiUrl = 'https://strapi-cloud-template-blog-ab0ea8f6c9.onrender.com/api';
   private tokenKey = 'jwt';
 
   constructor(private http: HttpClient) {}
