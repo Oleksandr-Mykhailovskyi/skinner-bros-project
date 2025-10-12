76d6076e3609edaa96adbea6f98eb61cb797d649
Oleksandr-Mykhailovskyi <Dadox56@gmail.com>
2025-10-12 10:56:34 +0200

    configuration

--- my-strapi-app/config/server.ts
+++ my-strapi-app/config/server.ts
@@ -1,7 +1,18 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+    remote: {
+      enabled: true,
+    },
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
